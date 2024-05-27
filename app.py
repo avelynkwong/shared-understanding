5be5136e3e9914e07696fd5a59a87daf3eab124f
avelynkwong <avelynkwong@gmail.com>
2024-05-27 14:34:55 -0400

    working home page view

--- app.py
+++ app.py
@@ -18,7 +18,6 @@ def get_paged_messages(bot_token, channel_id='C01BHE9FNQ6', start_time='0', end_
         json.dump(messages, f)
 
     # paging through time, each page contains maximum 100 messages
-    # 
     while has_more:
         page += 1
         prev_ts = messages[-1]['ts']
@@ -48,7 +47,58 @@ app = App(
 # for c in channels:
 #     print(c['name'], c['id'])
 
-get_paged_messages(bot_token)
+# get_paged_messages(bot_token)
+
+@app.event("app_home_opened")
+def update_home_tab(client, event, logger):
+    try:
+        # views.publish is the method that your app uses to push a view to the Home tab
+        client.views_publish(
+        # the user that opened your app's app home
+        user_id=event["user"],
+        # the view object that appears in the app home
+        view={
+            "type": "home",
+            "callback_id": "home_view",
+
+            # body of the view
+            "blocks": [
+            {
+                "type": "section",
+                "text": {
+                "type": "mrkdwn",
+                "text": "*Welcome to your _App's Home tab_* :tada:"
+                }
+            },
+            {
+                "type": "divider"
+            },
+            {
+                "type": "section",
+                "text": {
+                "type": "mrkdwn",
+                "text": "This button won't do much for now but you can set up a listener for it using the `actions()` method and passing its unique `action_id`. See an example in the `examples` folder within your Bolt app."
+                }
+            },
+            {
+                "type": "actions",
+                "elements": [
+                {
+                    "type": "button",
+                    "text": {
+                    "type": "plain_text",
+                    "text": "Click me!"
+                    }
+                }
+                ]
+            }
+            ]
+        }
+        )
+
+    except Exception as e:
+        logger.error(f"Error publishing home tab: {e}")
+
 
 # Ready? Start your app!
 if __name__ == "__main__":
