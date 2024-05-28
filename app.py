fd3c9a3ff021ba8b6d0c9bc738118f2870c3b3eb
avelynkwong <avelynkwong@gmail.com>
2024-05-28 13:29:07 -0400

    working dataframe without year

--- app.py
+++ app.py
@@ -2,8 +2,31 @@ import os
 import json
 import time
 from slack_bolt import App
+import pandas as pd
+from collections import defaultdict
 
-def get_paged_messages(bot_token, channel_id='C01BHE9FNQ6', start_time='0', end_time=str(time.time()), filename='dump'):
+def add_messages_to_df(df, msg_list, channel_id):
+    # dict to store each message instance
+    msg_dict = defaultdict(None)
+    for msg in msg_list:
+        msg_dict['year'] = None
+        msg_dict['channel_id'] = channel_id
+        msg_dict['channel_name'] = conversations[channel_id]
+        msg_dict['user_id'] = msg['user']
+        msg_dict['timestamp'] = msg['ts']
+        msg_dict['text'] = msg['text']
+        msg_dict['replies_cnt'] = msg.get('reply_count', 0)
+        reacts = msg.get('reactions', None)
+        reacts_cnt = 0
+        if reacts:
+            for react in reacts:
+                reacts_cnt += react['count']
+        msg_dict['reacts_cnt'] = reacts_cnt
+        df = pd.concat([df, pd.DataFrame([msg_dict])], ignore_index=True)
+    return df
+
+def get_paged_messages(df, bot_token, channel_id='C01BHE9FNQ6', start_time='0', end_time=str(time.time()), filename='dump'):
+
     history = app.client.conversations_history(
         token=bot_token,
         channel=channel_id,
@@ -12,14 +35,17 @@ def get_paged_messages(bot_token, channel_id='C01BHE9FNQ6', start_time='0', end_
     )
     messages = history['messages']
     has_more = history['has_more']
+    
+    df = add_messages_to_df(df, messages, channel_id)
 
-    page = 0
-    with open(f'{filename}_{page}.json', 'w') as f:
-        json.dump(messages, f)
+    # page = 0
+    # with open(f'{filename}_{page}.json', 'w') as f:
+    #     json.dump(messages, f)
+
 
     # paging through time, each page contains maximum 100 messages
     while has_more:
-        page += 1
+        # page += 1
         prev_ts = messages[-1]['ts']
         history = app.client.conversations_history(
             token=bot_token,
@@ -29,8 +55,11 @@ def get_paged_messages(bot_token, channel_id='C01BHE9FNQ6', start_time='0', end_
         )
         messages = history['messages']
         has_more = history['has_more']
-        with open(f'{filename}_{page}.json', 'w') as f:
-            json.dump(messages, f)
+        df = add_messages_to_df(df, messages, channel_id)
+    
+        # with open(f'{filename}_{page}.json', 'w') as f:
+        #     json.dump(messages, f)
+        return df
 
 
 bot_token = os.environ.get("SLACK_BOT_TOKEN")
@@ -41,13 +70,15 @@ app = App(
     signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
 )
 
-# Add functionality here later
-# @app.event("app_home_opened") etc.
-# channels = app.client.conversations_list(token=bot_token)['channels']
-# for c in channels:
-#     print(c['name'], c['id'])
+# list of conversations slack app has access to
+conversations = app.client.users_conversations(token=bot_token)['channels']
+conversations = {c['id']: c['name'] for c in conversations}
+print('The slack app has access to the following conversations: ')
+print(conversations)
 
-# get_paged_messages(bot_token)
+# df to store messages and metadata
+msg_df = pd.DataFrame()
+msg_df = get_paged_messages(msg_df, bot_token)
 
 @app.event("app_home_opened")
 def update_home_tab(client, event, logger):
